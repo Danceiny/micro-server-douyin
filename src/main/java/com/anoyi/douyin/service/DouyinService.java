9c0c5775d7b17522ce2d81a5b9933d7d8e6c41c0
anoy <545544032@qq.com>
2019-03-10 20:42:13 +0800

    Update

--- src/main/java/com/anoyi/douyin/service/DouyinService.java
+++ src/main/java/com/anoyi/douyin/service/DouyinService.java
@@ -26,9 +26,9 @@ public class DouyinService {
 
     private final static String XMLHttpRequest = "XMLHttpRequest";
 
-    private final static String VIDEO_LIST_API = "https://www.amemv.com/aweme/v1/aweme/post/?user_id=%s&count=21&max_cursor=%s&aid=1128&_signature=%s&dytk=%s";
+    private final static String VIDEO_LIST_API = "https://www.iesdouyin.com/aweme/v1/aweme/post/?user_id=%s&count=21&max_cursor=%s&aid=1128&_signature=%s&dytk=%s";
 
-    private final static String USER_SHARE_API = "https://www.amemv.com/share/user/%s?share_type=link";
+    private final static String USER_SHARE_API = "https://www.iesdouyin.com/share/user/%s?share_type=link";
 
     private final RpcNodeDyService rpcNodeDyService;
 
